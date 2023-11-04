04a86ff7e13f474ae085aed8bb60068c1c55a5ee
CharlesJub <charlie.jubera@gmail.com>
2023-11-04 01:10:02 -0400

    Regular auto-commit 2023-11-04 01:10:02

--- autocommit.bat
+++ autocommit.bat
@@ -2,8 +2,10 @@
 #!/bin/sh
 cd /c/Users/charl/Notes
 git add --all
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 
-git commit -am "Regular auto-commit %mydate%_%mytime%"
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+rem Extract date and time parts without milliseconds
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,2%
+
+git commit -am "Regular auto-commit %ldt%"
 git push origin master
