d7f9013b38d99a3b14aac585cc7cf734864bfdd4
CharlesJub <charlie.jubera@gmail.com>
2023-11-04 01:11:22 -0400

    Regular auto-commit

--- autocommit.bat
+++ autocommit.bat
@@ -1,11 +1,10 @@
 @echo off
+
+Set objShell = CreateObject("WScript.Shell")
+objShell.Run "your_script.bat", 0, True
+
 #!/bin/sh
 cd /c/Users/charl/Notes
 git add --all
-
-for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-rem Extract date and time parts without milliseconds
-set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,2%
-
-git commit -am "Regular auto-commit %ldt%"
+git commit -am "Regular auto-commit"
 git push origin master
