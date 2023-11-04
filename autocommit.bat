7030dc09cf74f361a6c5088cab6e471d3ad9bbec
CharlesJub <charlie.jubera@gmail.com>
2023-11-04 01:06:25 -0400

    Regular auto-commit $(2023-11-04_0106 AM)

--- autocommit.bat
+++ autocommit.bat
@@ -2,10 +2,7 @@
 #!/bin/sh
 cd /c/Users/charl/Notes
 git add --all
-
-timestamp() {
-  date +"at %H:%M:%S on %mm-%dd-%yy"
-}
-
-git commit -am "Regular auto-commit $(timestamp)"
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+git commit -am "Regular auto-commit $(%mydate%_%mytime%)"
 git push origin master
